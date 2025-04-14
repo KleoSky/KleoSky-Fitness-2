cca1120bccb9805b6741a7e8bce3a91049e3d964
Ankhena <ankhena@ya.ru>
2025-04-14 12:57:02 +0300

    Initial commit

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+import { switchTab } from './modules/price';
+switchTab();
+
+import { scroll } from './modules/hero';
+scroll();
+
+import { initSwiper } from './modules/juri';
+initSwiper();
+
+import { switchFaqTab, initAccordions } from './modules/faq';
+switchFaqTab();
+initAccordions();
+
+import { initFormValidation } from './modules/form-validate';
+initFormValidation();
+
+import { initVideo } from './modules/video';
+initVideo();
+
+import { initSwiperReviews } from './modules/reviews';
+initSwiperReviews();
